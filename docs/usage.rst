0257b5dff8867d5aa8cbc8341f508ca295fe47b9
Thierry Chappuis <tchappui@gmail.com>
2020-11-23 18:05:52 +0100

    Updated the documentation

--- docs/usage.rst
+++ docs/usage.rst
@@ -2,6 +2,16 @@
 Usage
 =====
 
-To use install-webdrivers in a project::
+To use install-webdrivers in a project, execute the install-webdrivers tool
+from the terminal. The following options are available::
 
-    import install_webdrivers
+    Usage: install-webdrivers [OPTIONS]
+
+    Script installing the latest versions of chromedriver and geckodriver
+    locally.
+
+    Options:
+    -p, --path TEXT  Specify the installation directory  [default: .]
+    --chromedriver   Only install chromedriver
+    --geckodriver    Only install geckodriver
+    --help           Show this message and exit.
--- webdrivers/cli.py
+++ webdrivers/cli.py
@@ -5,11 +5,18 @@ from . import webdrivers
 
 
 @click.command()
-@click.option('--path', '-p', default='.', show_default=True)
-@click.option('--chromedriver', is_flag=True)
-@click.option('--geckodriver', is_flag=True)
+@click.option(
+    '--path',
+    '-p',
+    default='.',
+    show_default=True,
+    help='Specify the installation directory',
+)
+@click.option('--chromedriver', is_flag=True, help='Only install chromedriver')
+@click.option('--geckodriver', is_flag=True, help='Only install geckodriver')
 def main(path, chromedriver, geckodriver):
-    """Console script for install_webdrivers."""
+    """Script installing the latest versions of chromedriver and geckodriver
+    locally."""
     if not geckodriver or (geckodriver and chromedriver):
         click.echo("Installing the latest version of chromedriver.")
         webdrivers.install_latest_chromedriver(path)
